c18e23f22ef925a0a35b7f3b54527c1d66a3c874
Zoranner <zoran_young@hotmail.com>
2025-03-20 15:01:51 +0800

    新增文件版本配置功能
    - 定义数据文件格式标识
    - 配置主次版本号参数
    - 设置默认时间戳配置
    - 优化索引间隔参数

---
+++ source/PcapFile.IO/Configuration/FileVersionConfig.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace KimoTech.PcapFile.IO.Configuration
+{
+    /// <summary>
+    /// 文件版本配置
+    /// </summary>
+    public static class FileVersionConfig
+    {
+        /// <summary>
+        /// PATA文件标识 ("PATA")
+        /// </summary>
+        public const uint PATA_MAGIC_NUMBER = 0x50415441;
+
+        /// <summary>
+        /// PCAP文件标识 ("PCAP")
+        /// </summary>
+        public const uint PCAP_MAGIC_NUMBER = 0xA1B2C3D4;
+
+        /// <summary>
+        /// 主版本号
+        /// </summary>
+        public const ushort MAJOR_VERSION = 1;
+
+        /// <summary>
+        /// 次版本号
+        /// </summary>
+        public const ushort MINOR_VERSION = 0;
+
+        /// <summary>
+        /// 默认时间戳精度(毫秒)
+        /// </summary>
+        public const uint DEFAULT_TIMESTAMP_ACCURACY = 1000;
+
+        /// <summary>
+        /// 默认索引间隔(毫秒)
+        /// </summary>
+        public const ushort DEFAULT_INDEX_INTERVAL = 1000;
+    }
+}
