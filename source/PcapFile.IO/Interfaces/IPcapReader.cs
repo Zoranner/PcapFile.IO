3e5c8d685ee2751244e81b25c26290f689078b47
Zoranner <zoran_young@hotmail.com>
2025-03-20 15:03:26 +0800

    新增数据读写基础接口
    - 实现数据包读取接口功能
    - 实现数据包写入接口功能
    - 支持同步异步双模式操作

---
+++ source/PcapFile.IO/Interfaces/IPcapReader.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KimoTech.PcapFile.IO
+{
+    /// <summary>
+    /// 数据读取接口
+    /// </summary>
+    public interface IPcapReader : IDisposable
+    {
+        #region 属性
+
+        /// <summary>
+        /// 获取文件头
+        /// </summary>
+        PcapFileHeader Header { get; }
+
+        /// <summary>
+        /// 获取已读取的数据包数量
+        /// </summary>
+        long PacketCount { get; }
+
+        /// <summary>
+        /// 获取文件的开始时间
+        /// </summary>
+        DateTime StartTime { get; }
+
+        /// <summary>
+        /// 获取文件的结束时间
+        /// </summary>
+        DateTime EndTime { get; }
+
+        /// <summary>
+        /// 获取当前读取位置
+        /// </summary>
+        long Position { get; }
+
+        /// <summary>
+        /// 是否已经到达文件末尾
+        /// </summary>
+        bool IsEndOfFile { get; }
+
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        long FileSize { get; }
+
+        /// <summary>
+        /// 当前文件路径
+        /// </summary>
+        string FilePath { get; }
+
+        /// <summary>
+        /// 文件是否已打开
+        /// </summary>
+        bool IsOpen { get; }
+
+        #endregion
+
+        #region 同步方法
+
+        /// <summary>
+        /// 打开数据文件
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns>是否成功打开</returns>
+        bool Open(string filePath);
+
+        /// <summary>
+        /// 关闭数据文件
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// 读取下一个数据包
+        /// </summary>
+        /// <returns>数据包，如果没有更多数据则返回空数据包</returns>
+        DataPacket ReadNextPacket();
+
+        /// <summary>
+        /// 跳转到指定时间
+        /// </summary>
+        /// <param name="timestamp">目标时间戳</param>
+        /// <returns>是否成功跳转</returns>
+        bool SeekToTime(DateTime timestamp);
+
+        /// <summary>
+        /// 跳转到指定位置
+        /// </summary>
+        /// <param name="position">目标位置</param>
+        /// <returns>是否成功跳转</returns>
+        bool Seek(long position);
+
+        /// <summary>
+        /// 重置读取位置到文件开始
+        /// </summary>
+        void Reset();
+
+        #endregion
+
+        #region 异步方法
+
+        /// <summary>
+        /// 异步读取下一个数据包
+        /// </summary>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns>数据包，如果没有更多数据则返回空数据包</returns>
+        Task<DataPacket> ReadNextPacketAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 异步跳转到指定时间
+        /// </summary>
+        /// <param name="timestamp">目标时间戳</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns>是否成功跳转</returns>
+        Task<bool> SeekToTimeAsync(
+            DateTime timestamp,
+            CancellationToken cancellationToken = default
+        );
+
+        #endregion
+    }
+}
---
+++ source/PcapFile.IO/Interfaces/IPcapWriter.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KimoTech.PcapFile.IO
+{
+    /// <summary>
+    /// 数据写入接口
+    /// </summary>
+    public interface IPcapWriter : IDisposable
+    {
+        #region 属性
+
+        /// <summary>
+        /// 获取已写入的数据包数量
+        /// </summary>
+        long PacketCount { get; }
+
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        long FileSize { get; }
+
+        /// <summary>
+        /// 获取当前文件路径
+        /// </summary>
+        string FilePath { get; }
+
+        /// <summary>
+        /// 是否自动刷新
+        /// </summary>
+        bool AutoFlush { get; set; }
+
+        /// <summary>
+        /// 文件是否已打开
+        /// </summary>
+        bool IsOpen { get; }
+
+        #endregion
+
+        #region 同步方法
+
+        /// <summary>
+        /// 创建新的数据文件
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="header">PCAP文件头</param>
+        /// <returns>是否成功创建</returns>
+        bool Create(string filePath, PcapFileHeader header = default);
+
+        /// <summary>
+        /// 打开现有数据文件进行追加
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns>是否成功打开</returns>
+        bool Open(string filePath);
+
+        /// <summary>
+        /// 关闭数据文件
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// 写入数据包
+        /// </summary>
+        /// <param name="packet">数据包</param>
+        /// <returns>是否成功写入</returns>
+        bool WritePacket(DataPacket packet);
+
+        /// <summary>
+        /// 批量写入数据包
+        /// </summary>
+        /// <param name="packets">数据包列表</param>
+        /// <returns>是否成功写入</returns>
+        bool WritePackets(IEnumerable<DataPacket> packets);
+
+        /// <summary>
+        /// 刷新缓冲区
+        /// </summary>
+        void Flush();
+
+        #endregion
+
+        #region 异步方法
+
+        /// <summary>
+        /// 异步写入数据包
+        /// </summary>
+        /// <param name="packet">数据包</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns>是否成功写入</returns>
+        Task<bool> WritePacketAsync(
+            DataPacket packet,
+            CancellationToken cancellationToken = default
+        );
+
+        /// <summary>
+        /// 异步批量写入数据包
+        /// </summary>
+        /// <param name="packets">数据包列表</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns>是否成功写入</returns>
+        Task<bool> WritePacketsAsync(
+            IEnumerable<DataPacket> packets,
+            CancellationToken cancellationToken = default
+        );
+
+        /// <summary>
+        /// 异步刷新缓冲区
+        /// </summary>
+        /// <param name="cancellationToken">取消令牌</param>
+        Task FlushAsync(CancellationToken cancellationToken = default);
+
+        #endregion
+    }
+}
